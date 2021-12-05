4de25f1186eb2118c0c30b554c0e01222aa75ce9
Jake Gaske <jakeco19@hotmail.com>
2021-12-05 11:13:35 +0800

    Updated Ignore

---
+++ LazyMeshCombine/Assets/Editor/LazyHelpers/LazySceneLoader/LazyScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LazyScene : ScriptableObject
+{
+	public List<LazySceneCategorys> activeCategorys = new List<LazySceneCategorys>();
+	public List<LazySceneCategorys> allCategorys = new List<LazySceneCategorys>();
+	
+	//public List<UnityEditor.SceneAsset> Scenes = new List<UnityEditor.SceneAsset>();
+}
---
+++ LazyMeshCombine/Assets/Editor/LazyHelpers/LazySceneLoader/LazySceneCategorys.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazySceneCategorys : ScriptableObject
+{
+    public string categoryTitle = "New";
+    public string categoryDescription = "Basic Description";
+    public List<UnityEditor.SceneAsset> Scenes = new List<UnityEditor.SceneAsset>();
+}
